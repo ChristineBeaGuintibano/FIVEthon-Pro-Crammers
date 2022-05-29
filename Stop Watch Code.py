38a6c1159d03fe06d68f2f5baa18fbd43ef5e7f4
Michael Angelo P. Villaruel <105473363+MicVill03@users.noreply.github.com>
2022-05-29 23:58:54 +0800

    Update Stop Watch Code.py

--- Stop Watch Code.py
+++ Stop Watch Code.py
@@ -2,8 +2,6 @@ from tkinter import *
 import time
 
 class StopWatch(Frame):  
-      
-
     def __init__(self, parent=None, **kw):        
         Frame.__init__(self, parent, kw)
         self.saved = []
