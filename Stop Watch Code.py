eed50b78315363711a57da53eb235373ad82b179
Michael Angelo P. Villaruel <105473363+MicVill03@users.noreply.github.com>
2022-05-30 14:41:58 +0800

    Update Stop Watch Code.py

--- Stop Watch Code.py
+++ Stop Watch Code.py
@@ -48,16 +48,18 @@ class StopWatch(Frame):
         self._timer = self.after(50, self._update)
     
     def _setTime(self, elap):
-        minutes = int(elap/60)
-        seconds = int(elap - minutes*60.0)
-        hseconds = int((elap - minutes*60.0 - seconds)*100)                
-        self.timestr.set('%02d:%02d:%02d' % (minutes, seconds, hseconds))
+        hours = int(elap/3600)
+        minutes = int((elap-hours*3600)/60)
+        seconds = int(elap-hours*3600-minutes*60)
+        hseconds = int((elap-hours*3600-minutes*60-seconds)*100)              
+        self.timestr.set('%02d:%02d:%02d:%02d' % (hours, minutes, seconds, hseconds))
 
     def _setLapTime(self, elap):
-        minutes = int(elap/60)
-        seconds = int(elap - minutes*60.0)
-        hseconds = int((elap - minutes*60.0 - seconds)*100)            
-        return '%02d:%02d:%02d' % (minutes, seconds, hseconds)
+        hours = int(elap/3600)
+        minutes = int((elap-hours*3600)/60)
+        seconds = int(elap-hours*3600-minutes*60)
+        hseconds = int((elap-hours*3600-minutes*60-seconds)*100)          
+        return '%02d:%02d:%02d:%02d' % (hours, minutes, seconds, hseconds)
         
     def Start(self):                                                     
         if not self._running:            
